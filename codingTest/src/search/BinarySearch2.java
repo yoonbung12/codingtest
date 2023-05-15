3894318f751141a35b9f68ffb5938344200b3deb
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-05-16 01:12:34 +0900

    Update BinarySearch2.java

--- codingTest/src/search/BinarySearch2.java
+++ codingTest/src/search/BinarySearch2.java
@@ -1,48 +1,70 @@
 package search;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class BinarySearch2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		//N레슨 개수, M블루레이 개수
-		//A기타 레슨 데이터 저장 배열
-		//start 이진탐색 시작 인덱스
-		//end 이진탐색 종료 인덱스
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		//N레슨 개수, M블루레이 개수 (9, 3)
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		//A기타레슨 데이터 저장 배열
 		int[] A = new int[N];
-		int start = 0;
-		int end = 0;
-		for(int i = 0; i < N; i++) {
-			A[i] = sc.nextInt();
-			if(start < A[i]) start = A[i];	//레슨 최대값을 시작 인덱스로 저장하기
-			end = end + A[i];				//모든 레슨의 총합을 종료 인덱스로 저장하기
+		
+		// 처음 인덱스,마지막 인덱스, 중앙값
+		int left = 0;
+		int right = 0;
+		int mid = (left + right)/2;
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i< N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+			
+			right += A[i];
+			left = Math.max(left, A[i]);
+			
+			while(left <= right) {
+				mid = (left + right)/2;
+				
+				int count = getCount(N, A, mid);
+				
+				if(count > M) {
+					left = mid + 1;
+				} else {
+					right = mid - 1;
+				}
+			}
 			
 		}
-		while(start <= end) {
-			int middle = (start + end)/2;
-			int sum = 0;
-			int count = 0;
-			for(int i = 0; i < N; i++) {	//middle값으로 모든 레슨을 저장 할 수 있는지 확인
-				if(sum + A[i] > middle) {
-					count++;
-					sum = 0;
-				}
-				sum = sum + A[i];
-				
-			}
-			if(sum != 0)	//탐색 후 sum이 0이 아니면 블루레이가 1개 더 필요하므로 더함
-				count++;
-			if(count > M)
-				start = middle + 1;
-			else
-				end = middle - 1;
-		}
-		System.out.println(start);
 		
+		System.out.println(left);
+		
+	}
+
+	private static int getCount(int N, int[] A, int mid) {
+
+		int sum = 0;
+		int count = 0;
+		for(int i = 0; i < N; i++) {
+			if(sum + A[i] > mid	) {
+				sum = 0;
+				count++;
+			}
+			sum += A[i];
+		}
+		if(sum != 0	) 
+			count++;
+		return count;
 	}
 
 }
