4e920deb43adbf81f14d8f7a00e54b22ba5e5d67
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-08-03 18:59:55 +0900

    합배열

--- codingTest/src/dataStructure/SumOfSection.java
+++ codingTest/src/dataStructure/SumOfSection.java
@@ -7,26 +7,28 @@ import java.util.StringTokenizer;
 
 public class SumOfSection {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException  {
+
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		//숫자 개수(N), 질의개수(M)
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		//합 배열
-		long[] S = new long[N + 1];
-		st = new  StringTokenizer(br.readLine());
-		for(int i =1; i <= N; i++) {
-			S[i] = S[i -1] + Integer.parseInt(st.nextToken());
+		//suno(숫자갯수), quizNo(질의 개수)
+		int suNo = Integer.parseInt(st.nextToken());
+		int quizNo = Integer.parseInt(st.nextToken());
+		long[] S = new long[suNo + 1];	//합배열을넣을 공간
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= suNo; i++) {
+			S[i] = S[i - 1] + Integer.parseInt(st.nextToken());	// 합배열 공식
+		
 		}
-		for(int q = 0; q < M; q++) {
+		for(int q = 0; q < quizNo; q++ ) {
 			st = new StringTokenizer(br.readLine());
 			int i = Integer.parseInt(st.nextToken());
 			int j = Integer.parseInt(st.nextToken());
-			//구간합 구하기
-			System.out.println(S[j] - S[i - 1]);
+			System.out.println(S[j] - S[i-1]);
 		}
+		
+		
 		
 		
 	}
