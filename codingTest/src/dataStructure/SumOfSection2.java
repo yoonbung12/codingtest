6c6d062623d2b8e41aefc1696e7f835cd2895704
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-08-07 18:27:56 +0900

    구간합2

--- codingTest/src/dataStructure/SumOfSection2.java
+++ codingTest/src/dataStructure/SumOfSection2.java
@@ -7,39 +7,48 @@ import java.util.StringTokenizer;
 
 public class SumOfSection2 {
 
-	public static void main(String[] args) throws IOException {
-		//N(배열의 크기), M(질의의 갯수) 저장
+	public static void main(String[] args) throws IOException  {
+		//11660_구간합 구하기2(빽준)
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		//N(배열 크기)=N * N , quizNo(질의 개수)
 		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int A[][] = new int[N+1][N+1]; // 인덱스를 0이 아닌  1부터 시작하기 위해서
-		for(int i = 1; i <= N; i++) {
+		int quizNo = Integer.parseInt(st.nextToken());
+		
+		//원본 배열 A[	][] 선언
+		int A[][] = new int[N+1][N+1];	// N+1은 배열의 시작을 0이아닌 1로하기위해
+		for(int i =1; i < N+1; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j=1; j <= N; j++) {
-				A[i][j] = Integer.parseInt(st.nextToken()); 
+			for(int j = 1; j < N+1; j++) {
+				A[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		//합배열 저장
-		int D[][] = new int[N+1][N+1];
-		for(int i = 1; i<= N; i++) {
-			for(int j = 1; j <= N; j++) {
-				D[i][j] = D[i][j-1] + D[i-1][j] - D[i-1][j-1] + A[i][j];
+		//구간합 구하기
+		int D[][]	= new int[N+1][N+1];
+		for(int i = 1; i < N+1; i++ ) {
+			for(int j = 1; j < N+1; j++ ) {
+				D[i][j]= D[i][j-1] + D[i-1][j] - D[i-1][j-1] + A[i][j];
 			}
 		}
-		//질의에 답변하기
-		for(int i = 0; i < M; i++) {
+		// 구간합 배열로 질의에 답변하기(quizNo)
+		for(int i = 0; i < quizNo; i++) {
 			st = new StringTokenizer(br.readLine());
-			// x1, y1, x2, y2
-			int x1 = Integer.parseInt(st.nextToken()); 
+			int x1 = Integer.parseInt(st.nextToken());
 			int y1 = Integer.parseInt(st.nextToken());
 			int x2 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
 			
-			//구간합 배열로 질의에 답변
-			int result = D[x1][y2] - D[x1-1][y2] - D[x2][y1-1] + D[x1-1][y1-1];
+			int result = D[x2][y2] - D[x2][y1-1] - D[x1-1][y2] + D[x1-1][y1-1];
 			System.out.println(result);
+			
 		}
+		
+		
+		
+			
 	}
+	
 
 }
