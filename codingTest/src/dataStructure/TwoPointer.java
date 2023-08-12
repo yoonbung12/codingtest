775b3ccb72586a3168d165cb8667814ce4032470
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-08-12 20:06:30 +0900

    투포인터

--- codingTest/src/dataStructure/TwoPointer.java
+++ codingTest/src/dataStructure/TwoPointer.java
@@ -1,34 +1,44 @@
 package dataStructure;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class TwoPointer {
 
-	public static void main(String[] args) {
-		// 연속된 자연수의 합 구하기(투 포인터 활용)
-		Scanner sc = new Scanner(System.in);
-		// N 변수 저장
-		int N = sc.nextInt();
-		// 사용변수 초기화(count = 1; start_index = 1; end_index = 1; sum = 1;)
-		int count = 1;
-		int start_index = 1;
+	public static void main(String[] args) throws IOException {
+		// 연속된 자연수의 합(빽2018)
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//StringTokenizer st = new StringTokenizer(br.readLine());	
+		//N변수 저장
+//		int N = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(br.readLine());
+
+		int count = 1;	//가지수
+		int sum = 1;	//합
+		// 투포인터 (startIndex,endIndex)
+		int start_index = 1; 
 		int end_index = 1;
-		int sum = 1;
 		
-		while(end_index != N) {	
-			if(sum == N) {	// 햔제 얀속 합이 N과 같은 경우
-				count ++;
-				end_index ++;
+		while(end_index != N) { //endindex가 N가 같아지지 않을때까지 반복
+			if(sum == N	) {	//합이 N가 같은수가 나오면 count++해줌.
+				count++;
+				end_index++;
 				sum = sum + end_index;
-			} else if(sum > N) { // 현재 연속 합이 N보다 더 큰 경우
+			} else if(sum > N) {
 				sum = sum - start_index;
 				start_index++;
 			} else {
 				end_index++;
 				sum = sum + end_index;
+				
 			}
 		}
-			System.out.println(count);
+		System.out.println(count);
+		
+		
+		
 	}
 
 }
