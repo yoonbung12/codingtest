8686fa5f4f11119bed4a37b147b4cbffa4b63d9c
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-08-21 18:42:52 +0900

    주몽

--- codingTest/src/dataStructure/TwoPointer2.java
+++ codingTest/src/dataStructure/TwoPointer2.java
@@ -1,42 +1,47 @@
 package dataStructure;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class TwoPointer2 {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// 주몽의 명령 문제 구하기
+	public static void main(String[] args) throws IOException {
+		// 주몽의 명령 문제 구하기(백준 1940)
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// N재료의 개수, M갑옷이 되는 번호,
-		int N = Integer.parseInt(br.readLine());
-		int M = Integer.parseInt(br.readLine());
-		int[] A = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		//N(재료 갯수), M(갑옷이 만들어지는 재료합수)		
+		int N = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[] A = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
 			A[i] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(A); //오름차순 정렬
+		Arrays.sort(A);
+		
+		//count는 합이 9가 되는 횟수
 		int count = 0;
-		int i = 0;
-		int j = N - 1;
-		while(i < j) { //투 포인터 이동원칙에 따라 포인터를 이동하며 처리
-			if(A[i] + A[j] < M) {
+		int i = 0;	//배열 시작 번호
+		int j = N-1; //배열 마지막 번호
+		while(i < j) {
+			if(A[i] + A[j] < M) { //두 합이 M 보다 작으면
 				i++;
-			} else if(A[i] + A[j] > M) {
+			} else if(A[i] + A[j] > M) { //두합이 M보다 크면
 				j--;
-			} else {
+			} else{
+				i++;
+				j--;
 				count++;
-				i++;
-				j--;
-				
+			
 			}
+			
 		}
 		System.out.println(count);
-		br.close();
 
 	}
 
