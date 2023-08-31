f1b17ee334dba4ef4c2475e08eaf4abafe0a385d
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-08-31 17:49:23 +0900

    슬라이딩 윈도우(다시)

--- codingTest/src/dataStructure/SlidingWindow2.java
+++ codingTest/src/dataStructure/SlidingWindow2.java
@@ -1,35 +1,29 @@
 package dataStructure;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 import com.sun.corba.se.impl.orbutil.graph.Node;
 
+import java.io.*;
+
+
 public class SlidingWindow2 {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException  {
 		// 최솟값 찾기(슬라이딩 윈도우)-11003번
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		//출력을 버퍼에 넣고 한 번에 출력하기 위해 BufferedWriter 사용
+		// 출력을 버퍼에 넣고 한번에 출력 할려고BufferedWriter 사용
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		// N(데이터 개수), L(최솟값을 구하는 범위)
+		//N(데이터 개수), L(최솟값을 구하는 범위)
 		int N = Integer.parseInt(st.nextToken());
 		int L = Integer.parseInt(st.nextToken());
+		
 		st = new StringTokenizer(br.readLine());
-		// Deque<Node> mydeque(데이터를 담을 덱 자료구조)
-		Deque<Node> mydeque = new LinkedList<>();
-
-		for(int i = 0; i< N; i++) {
-			int now = Integer.parseInt(st.nextToken());
-			//새로운 값이 들어올 때마다 정렬 대신 현재 수보다 큰 값을 덱에서 제거해 시간 복잡도를 줄임
+		Deque<Node> mydeque = new LinkedList<>(); //데이터 담는 덱 구조
+		for(int i  = 0; i < N; i++) {
+			int now = Integer.parseInt(st.nextToken()); //현재데이터 값
 			
 			while(!mydeque.isEmpty() && mydeque.getLast().value > now) {
 				mydeque.removeLast();
@@ -37,16 +31,17 @@ public class SlidingWindow2 {
 			mydeque.addLast(new Node(now, i));
 			
 			//범위에서 벗어난 값은 덱에서 제거
-			if(mydeque.getFirst().index <= i - L	) {
+			if(mydeque.getFirst().index <= i - L) {
 				mydeque.removeFirst();
 			}
 			bw.write(mydeque.getFirst().value + " ");
-			
 		}
 		bw.flush();
 		bw.close();
 		
-
+		
+		
+		
 	}
 	static class Node{
 		public int value;
@@ -56,6 +51,7 @@ public class SlidingWindow2 {
 			this.value = value;
 			this.index = index;
 		}
+		
 	}
 
 }
