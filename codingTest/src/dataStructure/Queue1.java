54675e41173a14ec9182370a2a37e18968807c26
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-09-06 16:35:58 +0900

    queue

--- codingTest/src/dataStructure/Queue1.java
+++ codingTest/src/dataStructure/Queue1.java
@@ -1,30 +1,31 @@
 package dataStructure;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+
 
 public class Queue1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		// 카드 게임(2164번)
-		// N(카드의 개수), myQueue(카드 저장 자료구조)
-		Scanner sc = new Scanner(System.in);
-		//Queue선언(int)값 받을것
-		Queue<Integer> myQueue = new LinkedList<>();
-		int N = sc.nextInt();
-		for(int i = 1; i < N; i++) { //카드를 큐에 저장하기
-			myQueue.add(i);
-			 
+		//N(카드의 개수), queue(카드 저장하는 구조)
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		Queue<Integer> queue = new LinkedList<>();
+		
+		for(int i = 1; i <= N; i++) {
+			queue.add(i);
 		}
-		//카드가 한장남을때까지 while문 반복
-		while(myQueue.size() > 1	) {
-			myQueue.poll(); //맨위에 카드를 버림
-			//int temp = myQueue.poll(); 26번줄이랑 같은거임(쓰는방식만  다를뿐)
-			//myQueue.add(temp);
-			myQueue.add(myQueue.poll());
+		while(queue.size() > 1) { //큐에 한장만 남게되면 while문 빠져나옴
+			queue.poll(); //맨위 카드 버림
+			queue.add(queue.poll()); // 맨위에 폴하고 다음 맨위 카드를 맨 아래로 이동
+			
 		}
-		System.out.println(myQueue.poll());
+		System.out.println(queue.poll()); //마지막으로 남은 카드 출력
+		
 	}
 
 }
