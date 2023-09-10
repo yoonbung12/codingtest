d5ea50592d3dae511a57777888da8b530c148d7a
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-09-10 18:33:13 +0900

    버블 정렬

--- codingTest/src/sort/BubbleSort.java
+++ codingTest/src/sort/BubbleSort.java
@@ -1,37 +1,34 @@
 package sort;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class BubbleSort {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		// 수 정렬하기(2750)
-		// N개의 수, A[] 정렬할 배열 선언
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//N(정렬할 수 개수)
+		int N = Integer.parseInt(br.readLine());
+		//A(정렬할 배열 선언)
 		int A[] = new int[N];
-		// A의 값을 받을것
-		for(int i = 0; i < N; i++) {
-			A[i] = sc.nextInt();
+		for(int i = 0; i<N; i++) {
+			A[i] = Integer.parseInt(br.readLine());
 		}
-		//버블 정렬 구현(이중 for문 쓸것)
-		for(int i = 0; i < N -1; i++) { //루프가 몇번 도는지
+		for(int i = 0; i < N - 1; i++) {
 			for(int j = 0; j < N - 1 - i; j++) {
-				if(A[j] > A[j + 1]) { // 인접한 두수 비교(앞에것이 더 크다는 가정)
-					// swap방법
-					int temp = A[j]; 
+				if(A[j] > A[j-1]) {
+					int temp = A[j];
 					A[j] = A[j + 1];
 					A[j + 1] = temp;
-					
-					
 				}
-				
 			}
-			
 		}
 		for(int i = 0; i < N; i++) {
 			System.out.println(A[i]);
 		}
+		
 
 	}
 
