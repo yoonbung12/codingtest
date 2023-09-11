1cbc15009415250c9c0085e7b4ac74d9c06481b4
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-09-11 17:24:46 +0900

    다시(BubbleSort)

--- codingTest/src/sort/SortMaxValue.java
+++ codingTest/src/sort/SortMaxValue.java
@@ -8,42 +8,46 @@ import java.util.Arrays;
 public class SortMaxValue {
 
 	
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws NumberFormatException, IOException  {
 		// 버블소트 정렬구하기(1377번)
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		// N(데이터 개수), A[](데이터 배열, 단 클래스를 데이터롤 담는 배열
-		int N = Integer.parseInt(reader.readLine());
-		mData[] A = new mData[N];
-		//for(N만큼 반복하기)
-		for(int i = 0; i < N; i++) {
-			A[i] = new mData(Integer.parseInt(reader.readLine()), i);
-		}
-		//A배열 정렬하기
-		Arrays.sort(A);	//	A배열 정렬(O(nlogN)시간 복잡도
-		int Max = 0;
-		//for(N만큼 정렬하기)
-		for(int i = 0; i < N; i++) {	//정렬전 index - 정렬후 index 계산의 최댓값 저장하기
-			if(Max < A[i].index - i)
-				Max = A[i].index - i;
-		}
-		System.out.println(Max + 1);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//데이터 개수(N), A(데이터 배열, 단 클래스를 데이터로 담는 배열)
+		int N = Integer.parseInt(br.readLine());
+		cData[] A = new cData[N]; 
 		
+		//for(N만큼 반복)
+		for(int i = 0; i< N; i++) {
+			A[i] = new cData( Integer.parseInt(br.readLine()), i);
+		}
+		
+		Arrays.sort(A); //A 배열 정렬하기(O(nlogn)시간 복잡도
+		int Max = 0;
+		for(int i = 0; i < N; i++) {
+			//A[i]의 정렬전 index - 정렬후 index 계산의 최댓값을 찾아 저장
+			if(Max < A[i].index - i) {
+				Max = A[i].index - i;
+			}
+		}
+		System.out.println(Max + 1);		
 	}
 
 }
-//별도 클래스 선언
-class mData implements Comparable<mData>{
+
+class cData implements Comparable<cData> {
+	
 	int value;
 	int index;
-	
-	public mData(int value, int index) { //mData생성자 , 매개변수 생성
-		super();
+
+	public cData(int value, int index) {
 		this.value = value;
 		this.index = index;
 	}
+
 	@Override
-	public int compareTo(mData o) {
+	public int compareTo(cData o) { //value기준 오름차순
 		return this.value - o.value;
-		
 	}
+	
 }
+
+
