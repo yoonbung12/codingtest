f9b8459fbdad6ec1eceb1bfba929a1e20f4a57ed
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-09-11 17:17:35 +0900

    bublleSort수정

--- codingTest/src/sort/BubbleSort.java
+++ codingTest/src/sort/BubbleSort.java
@@ -18,7 +18,7 @@ public class BubbleSort {
 		}
 		for(int i = 0; i < N - 1; i++) {
 			for(int j = 0; j < N - 1 - i; j++) {
-				if(A[j] > A[j-1]) {
+				if(A[j] > A[j+1]) {
 					int temp = A[j];
 					A[j] = A[j + 1];
 					A[j + 1] = temp;
