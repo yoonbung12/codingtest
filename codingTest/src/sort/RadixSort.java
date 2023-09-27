d89f7e0fb7ec7919197f20f6217d4112f1c97cb3
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-09-27 15:05:28 +0900

    기수 정렬
    기수정렬 활용

--- codingTest/src/sort/RadixSort.java
+++ codingTest/src/sort/RadixSort.java
@@ -8,65 +8,54 @@ import java.io.OutputStreamWriter;
 
 public class RadixSort {
 
-	private static int[]A;
-	private static long result;
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// 수 정렬하기3(10989번)
+	private static int[] A;		//정렬할 배열(A)
+	
+
+	public static void main(String[] args) throws NumberFormatException, IOException  {
+		// 수 정렬하기3(10989번)-기수 정렬 활용 
+
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		// N(정렬할 수 갯수), A(정렬할 배열 선언하기)
+		
+		//정렬할 수 갯수(N)
 		int N = Integer.parseInt(br.readLine());
 		A = new int[N];
-		//for(N의 개수만큼 반복{A배열 저장하기}
 		for(int i = 0; i < N; i++) {
 			A[i] = Integer.parseInt(br.readLine());
 		}
 		br.close();
-		Radix_Sort(A, 5);
-		for(int i = 0; i < N; i++) {
-			bw.write(A[i] + "\n");
-		}
-		bw.flush();
-		bw.close();
-		//기수 정렬 함수 수행하기
-		//정렬된 A배열 출력하기
-
-	}
-	//기수 정렬 함수 구현하기
-	//변수 선언 부
-	//bucket(현재 자릿수들의 분포를 합 배열의 형태로 알려 주는 배열)
-	//ex: bucket[4] -> 현재 기준 자릿값이 0 ~ 4까지 몇 개의 데이터가 있는지 저장하기
-	//output(임시 정렬을 위한 배열)
-	//jarisu(현재 자릿수를 표현하는 수)
+		Radix_sort(A, 5); // 5는 만의 자릿수보자 작아서 5를 적었다.
+		
 	
-	//로직 부분
-	private static void Radix_Sort(int[] A, int max_size) {
-		int[] output = new int[A.length];
-		int jarisu = 1;
+		
+	}
+
+
+	private static void Radix_sort(int[] A, int max_size) {
+		int[] output = new int[A.length]; //임시정렬을 위한 배열 
+		int jarisu = 1;	//현재 자릿수
 		int count = 0;
-		//while(최대 자릿수 만큼 반복하기)
-		while(count != max_size) {
-			// 현재 기준 자릿수를 기준으로 A배열 데이터를 bucket 에 count
-			//합 배열 공식으로 bucket을  합 배열 형태로 변경하기
+		//로직 부분
+		while(count != max_size) { //최대 자릿수 만큼 반복하기
 			int[] bucket = new int[10];
 			for(int i = 0; i < A.length; i++) {
-				bucket[(A[i] / jarisu) % 10]++;	//일의 자리부터 시작하기
+				bucket[(A[i] / jarisu) % 10]++; //일의 자리부터 시작하기 
 			}
-			for(int i = 1; i < 10; i++) {	//합 배열을 이용해 index계산하기
-				bucket[i] += bucket[i - 1];
+			for(int i = 1; i < 10; i++) {	//합배열 사용해서 index계산
+				bucket[i] = bucket[i] + bucket[i - 1];
 			}
 			for(int i = A.length - 1; i >= 0; i--) { //현재 자릿수를 기준으로 정렬하기
-				output[bucket[(A[i] /jarisu % 10)] - 1] = A[i];
+				output[bucket[(A[i] / jarisu % 10)] - 1] = A[i];
 				bucket[(A[i] / jarisu) % 10]--;
-				
-				
 			}
 			for(int i = 0; i < A.length; i++) {
 				//다음 자릿수를 이동하기 위해 현재 자릿수 기준 정렬 데이터 저장하기
 				A[i] = output[i];
 			}
-			jarisu = jarisu * 10;	//자릿수 증가시키기
+			jarisu = jarisu * 10; //자릿수 증가시키기
 			count++;
+			
 		}
 		
 	}
