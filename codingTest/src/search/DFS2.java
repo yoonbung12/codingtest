e45e2d7c18a82c470a21245295d315666a79df52
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-09-30 15:09:33 +0900

    DFS:재귀함수 활용

--- codingTest/src/search/DFS2.java
+++ codingTest/src/search/DFS2.java
@@ -1,24 +1,29 @@
 package search;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class DFS2 {
-
+	
 	static int N;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		//신기한 소수 찾기2023번
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
 
-		Scanner in = new Scanner(System.in);
-		N = in.nextInt();
-		
-		DFS(2, 1);	//일의 자리 소수는 2,3,5,7이므로 4개 수에서만 시작
+		//DFS실행하기
+		DFS(2, 1);
 		DFS(3, 1);
 		DFS(5, 1);
 		DFS(7, 1);
+		
+		
 	}
-	//DFS구현
-	static void DFS(int number, int jarisu) {
+
+	//DFS(숫자, 자릿수)
+	private static void DFS(int number, int jarisu) {
 		if(jarisu == N) {
 			if(isPrime(number)) {
 				System.out.println(number);
@@ -26,20 +31,24 @@ public class DFS2 {
 			return;
 		}
 		for(int i = 1; i < 10; i++) {
-			if(i % 2 == 0) {	//짝수라면 더 이상 탐색할 필요가 없음
+			if(i % 2 == 0) { //짝수라면 더이상 탐색할 필요가 없다.
 				continue;
 			}
-			if(isPrime(number * 10 +i)) {	//소수라면 재귀 함수로 자릿수 늘림
+			if(isPrime(number * 10 + i)) { //소수라면 재귀 함수로 자릿수를 늘린다.
 				DFS(number * 10 + i, jarisu + 1);
 			}
 		}
-	}
-	static boolean isPrime(int num) {
-		for(int i = 2; i <= num / 2; i++)
-			if(num % i == 0)
-				return false;
-		return true;
 		
+	}
+
+	 static boolean isPrime(int num) {
+		for(int i =2; i <= num /2; i++) {
+			if(num % i == 0) {
+				return false;
+			}
+		}
+		return false;
+
 	}
 	
 
