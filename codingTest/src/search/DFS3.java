139d88a0be928a0e85767f4f6ace91904114f916
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-10-02 15:54:31 +0900

    DFS: 다시볼것

--- codingTest/src/search/DFS3.java
+++ codingTest/src/search/DFS3.java
@@ -1,36 +1,44 @@
 package search;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class DFS3 {
 
-	static boolean visited[]; //방문 기록 배열
-	static ArrayList<Integer>[]A; //인접리스트
-	static boolean arrive; //도착 확인 변수
-
+	static boolean visited[]; // 방문기록 저장하는 배열
+	static boolean arrive; // 도착확인 변수
+	static ArrayList<Integer>[] A;
 	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws IOException {
 		// 친구관계 파악하기13023번
-		//n(노드개수),m(엣지 개수)
-		int N;
-		int M;
 		arrive = false;
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		//n(노드개수),m(엣지 개수), A(그래프 데이터 저장 인접 리스트)
+		int N = Integer.parseInt(st.nextToken()); 
+		int M = Integer.parseInt(st.nextToken());
 		A = new ArrayList[N];
 		visited = new boolean[N];
-		for(int i = 0; i < N; i++) {	//A인접 리스트의 각 ArrayList초기화
+		
+		for(int i = 0; i < N; i++) {
+			//A인접 리스트의 각 ArrayList초기화 하기
 			A[i] = new ArrayList<Integer>();
+			
 		}
-		for(int i = 0; i < M;) {	//각 노드마다 DFS실행
-			int S = sc.nextInt();
-			int E = sc.nextInt();
+		
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int S = Integer.parseInt(st.nextToken());
+			int E = Integer.parseInt(st.nextToken());
 			A[S].add(E);
 			A[E].add(S);
 		}
-		for(int i = 0; i < N; i++) {
+		for(int i = 0; i< N; i++) {
 			DFS(i, 1);	//depth 1부터 시작
 			if(arrive)
 				break;
@@ -38,22 +46,27 @@ public class DFS3 {
 		if(arrive)
 			System.out.println("1");
 		else
-			System.out.println("0"); // 5의 깊이가 없다면 0 출력
-	}
+			System.out.println("0");
+		
+		
+		
+			}
 
-	//DFS구현
-	private static void DFS(int now, int depth) {
-		if(depth == 5 || arrive) {	//depthrk 5가 되면 프로그램 종료
+
+	private static void DFS(int now, int depth) { //DFS구현하기
+		if(depth == 5 || arrive) { //depth가 5가되면 프로그램 종료
 			arrive = true;
 			return;
 		}
 		visited[now] = true;
 		for(int i : A[now]) {
 			if(!visited[i]) {
-				DFS(i, depth + 1);	//재귀 호출이 될때마다 depth를 1씩 증가
+				DFS(i, depth + 1); //재귀 호출이 될때 마다 depth를 1씩 증가
+				
 			}
 		}
 		visited[now] = false;
+		
 	}
 
 }
