abb7302b62ca1820285330dd8c2cb00d12cc3388
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-10-08 18:52:31 +0900

    BFS:지름 구하기(다시)

--- codingTest/src/search/BFSDiameter.java
+++ codingTest/src/search/BFSDiameter.java
@@ -1,54 +1,66 @@
 package search;
 
-//import java.util.ArrayList;
-//import java.util.Scanner;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class BFSDiameter {
-	//A(인접 리스트), 
+	//A(인접 리스트)ArrayList<edge>로 선언할것 
+	static ArrayList< Edge>[] A;
 	//visited(방문 기록 저장 배열), distance(거리 저장 배열)
 	static boolean visited[];
 	static int[] distance;
-	static ArrayList< Edge>[] A;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// p1167트리의 지름
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();		//노드갯수(n),
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		//노드의 개수 N
+		int N = Integer.parseInt(st.nextToken());
 		A = new ArrayList[N + 1];
-		for(int i = 1; i <= N; i++) {	//A의 인접 리스트의 각 ArrayList 초기화하기
-			A[i] = new ArrayList< Edge>();
+		for(int i = 1; i <= N; i++) {
+			A[i]  = new ArrayList<Edge>(); //ArrayList초기화 하기
 			
 		}
-		for(int i = 0; i < N; i++) {	//A 인접 리스트에 그래프 데이터 저장하기
-			int S = sc.nextInt();
+		for(int i = 0; i < N; i++) { //A인접 리스트에 그래프 데이터 저장하기
+			st = new StringTokenizer(br.readLine());
+			int S = Integer.parseInt(st.nextToken());
 			while(true) {
-				int E = sc.nextInt();
+				st = new StringTokenizer(br.readLine());
+				int E = Integer.parseInt(st.nextToken());
 				if(E == -1)
 					break;
-				int V = sc.nextInt();
+				int V = Integer.parseInt(st.nextToken());
 				A[S].add(new Edge(E, V));
 			}
 			
 		}
-		distance = new int[N + 1];	//배열 초기화 하기
-		visited = new boolean[N + 1];//배열 초기화 하기
-
-		BFS(1);	//임의의 시작점으로 실행
+		distance = new int[N + 1];
+		visited = new boolean[N + 1];
+		BFS(1);
 		int Max = 1;
-		for(int i = 2; i <= N; i++) {	//distance 배열에서 가장 큰 값으로 다시 시작점 설정
+		for(int i = 2; i <= N; i++) { //distance배열에서 가장 큰 값으로 다시 시작점 설정
 			if(distance[Max] < distance[i])
 				Max = i;
 		}
-		distance = new int[N + 1];//배열 초기화 하기
-		visited = new boolean[N + 1];//배열 초기화 하기
+		
+		distance = new int[N + 1];
+		visited = new boolean[N + 1];
 		BFS(Max);	//새로운 시작점으로 실행
 		Arrays.sort(distance);
 		System.out.println(distance[N]);
+			
+		}
+		
+		
 
-	}
-
-	private static void BFS(int index) {	//BFS구현
+	private static void BFS(int index) { //BFS구현
 		Queue<Integer> queue = new LinkedList<Integer>();
 		queue.add(index);
 		visited[index] = true;
@@ -61,16 +73,14 @@ public class BFSDiameter {
 					visited[e] = true;
 					queue.add(e);
 					distance[e] = distance[now_node] + v; //거리 배열 업데이트 하기
-					
 				}
 			}
 		}
-		
-	}
 	
+	}
 }
 
-class Edge{
+class Edge {
 	int e;
 	int value;
 	public Edge(int e, int value) {
