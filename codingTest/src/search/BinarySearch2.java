c0fe3e98ced679023dc2c7c8e634ee44c3a68b3c
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-10-13 14:34:41 +0900

    이진탐색

--- codingTest/src/search/BinarySearch2.java
+++ codingTest/src/search/BinarySearch2.java
@@ -1,70 +1,58 @@
 package search;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class BinarySearch2 {
 
-	public static void main(String[] args) throws IOException {
-
+	public static void main(String[] args) throws IOException  {
+		//블루레이 구하기 2343(백준)
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		//N레슨 개수, M블루레이 개수 (9, 3)
+		//N(레슨 개수), M(블루레이 개수)
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		//A기타레슨 데이터 저장 배열
+		//A(기타레슨 데이터 저장배열)
 		int[] A = new int[N];
-		
-		// 처음 인덱스,마지막 인덱스, 중앙값
-		int left = 0;
-		int right = 0;
-		int mid = (left + right)/2;
+		// start(이진탐색 시작 인덱스), end(이진탐색 종료 인덱스)
+		int start = 0;
+		int end = 0;
 		
 		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i< N; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
-			
-			right += A[i];
-			left = Math.max(left, A[i]);
-			
-			while(left <= right) {
-				mid = (left + right)/2;
-				
-				int count = getCount(N, A, mid);
-				
-				if(count > M) {
-					left = mid + 1;
-				} else {
-					right = mid - 1;
-				}
-			}
-			
-		}
-		
-		System.out.println(left);
-		
-	}
-
-	private static int getCount(int N, int[] A, int mid) {
-
-		int sum = 0;
-		int count = 0;
 		for(int i = 0; i < N; i++) {
-			if(sum + A[i] > mid	) {
-				sum = 0;
+			//A 배열 저장
+			A[i] = Integer.parseInt(st.nextToken());
+			// 시작 인덱스 저장(A배열의 최댓값), 종료 인데스 저장(A배열의 총합)
+			if(start < A[i] ) start = A[i]; //레슨 최댓값을 시작 인덱스로 저장하기
+			end = end + A[i]; //모든 레슨의 총합을 종료 인덱스로 저장하기
+					
+		}
+		//while(시작 인덱스 <= 종료인덱스) 
+		while(start <= end) {
+			int middle = (start + end) / 2; //중간 인덱스(middle)
+			int sum = 0; //레슨합
+			int count = 0; // 현재 사용한 블루레이 개수
+			
+			//middle값으로 모든 레슨을 저장할 수 있는지 확인
+			for(int i = 0; i < N; i++) {
+				if(sum + A[i] > middle) {
+					count++;
+					sum = 0; //sum 초기화 시키기
+				}
+				sum = sum + A[i];
+			}
+			// 탐색 후 sum 이 0이 아니면 블루레이가 1개 더 필요하니까 더함
+			if(sum != 0) {
 				count++;
 			}
-			sum += A[i];
+			if(count > M) {
+				start = middle+1;
+			} else {
+				end = middle - 1;
+			}
 		}
-		if(sum != 0	) 
-			count++;
-		return count;
+		System.out.println(start);
 	}
-
 }
